ceeb22745a6b51fb74a0c2ed6b4f2ab0700382a9
Myoxsis <myoxsis@gmail.com>
2024-09-12 22:12:32 +0200

    Adding Stock.py

---
+++ Stock.py
@@ -0,0 +1,8 @@
+class Stock():
+    def __init__(self, name, market, actual_price):
+        self.name = name
+        self.market = market
+        self.actual_price = actual_price
+
+    def set_actual_price(price)
+

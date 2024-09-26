83171d37266632b4d2b6c32ee1247c32ed163371
maxime_alain <69240270+Myoxsis@users.noreply.github.com>
2024-09-26 19:04:44 +0200

    Update Limits

--- test_trading_bot_v2.py
+++ test_trading_bot_v2.py
@@ -73,21 +73,14 @@ class Strategy:
 #%%
 
 CASH_ON_HAND = 1000
-
-
-for current
-
-alo_ord = Order(alo)
-alo_ord.create_order("buy", 2)
-alo_ord.submit_order()
-
-
-#%%
+ORDER_TO_MANAGE = 2
+LOW_LIMIT_PERCENT = 1
+HIGH_LIMIT_PERCENT = 2
+LAST_TRADE_VAL = 0
 
 # initialize
 
 alo = Asset("ALO.PA", "Alstom S.A.")
-
 alo_position = Position(alo)
 alo_position.display_position()
 
@@ -98,52 +91,56 @@ last_trade = None
 date_l = []
 quote_l = []
 trade_l = []
+cash_on_hand_l = []
 
 quote = 5
-LAST_TRADE_VAL = 0
+
 for current_date in pd.date_range(datetime(2023, 10, 8), datetime(2023, 12, 5)):
     quote = quote + (1 * (np.random.randint(-4, 4)/100) * (np.random.randint(2, 5)/10) + pow((0.0005/10),6) ) 
     date_l.append(current_date)
     quote_l.append(quote)
     last_trade_val = 0
 
-    print(current_date, quote)
-
     if last_trade == None:
-        if quote < 4.98:
+        if quote < 5.00:
             alo_ord = Order(alo)
-            alo_ord.create_order("buy", 2)
+            alo_ord.create_order("buy", ORDER_TO_MANAGE)
             alo_ord.submit_order()
             last_trade = "buy"
             LAST_TRADE_VAL = quote
             trade_l.append(quote)
+            CASH_ON_HAND = CASH_ON_HAND - (quote * alo_ord.quantity)
         else:
             trade_l.append(None)
     
     elif last_trade == "buy":
-        print("Trade past : ", LAST_TRADE_VAL)
-        target_price = LAST_TRADE_VAL - (LAST_TRADE_VAL / 1000)
-        print("Target Price : ", target_price)
-        if quote <= target_price:
+        low_limit_price = LAST_TRADE_VAL * 1-(LOW_LIMIT_PERCENT/100)
+        high_limit_price = LAST_TRADE_VAL * 1+(HIGH_LIMIT_PERCENT/100)
+        if quote <= low_limit_price:
             alo_ord = Order(alo)
-            alo_ord.create_order("sell", 2)
+            alo_ord.create_order("sell", ORDER_TO_MANAGE)
             alo_ord.submit_order()
             last_trade = "sell"
             trade_l.append(quote)
+            CASH_ON_HAND = CASH_ON_HAND + (quote * alo_ord.quantity)
+        elif quote >= high_limit_price:
+            alo_ord = Order(alo)
+            alo_ord.create_order("sell", ORDER_TO_MANAGE)
+            alo_ord.submit_order()
+            last_trade = "sell"
+            trade_l.append(quote)
+            CASH_ON_HAND = CASH_ON_HAND + (quote * alo_ord.quantity)
         else:
             trade_l.append(None)
     else:
         trade_l.append(None)
-
-
+    cash_on_hand_l.append(CASH_ON_HAND)
 
 plt.plot(date_l, quote_l)
 plt.scatter(date_l, trade_l)
 
-
-len(trade_l)
-len(date_l)
-
-alo_position.display_position()
+#alo_position.display_position()
 
 print(trade_l)
+print(cash_on_hand_l)
+print(f"TOTAL EARNING : ", cash_on_hand_l[-1] - 1000)
